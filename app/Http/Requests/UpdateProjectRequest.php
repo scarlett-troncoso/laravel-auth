393607f2533f9f527f1244aaeb6d9af42f20f21d
scarlett-troncoso <scar.labussieri@gmail.com>
2024-05-26 14:43:58 +0200

    active abilitation and add validation

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -11,7 +11,7 @@ class UpdateProjectRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class UpdateProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required',
+            // 'cover_image' => 'nullable|image|max:500'
+            'description' => 'nullable',
+            'tools' => 'nullable'
         ];
     }
 }
