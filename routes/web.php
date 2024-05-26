cd274e398927de084ccfc1bd634af6a8619db215
scarlett-troncoso <scar.labussieri@gmail.com>
2024-05-26 20:52:07 +0200

    add routes for index and show of guests pages

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Project;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['projects' => Project::orderByDesc('id')->take(3)->get()]);
 });
+
+Route::get('projects', function () {
+    return view('guests.projects.index', ['projects' => Project::orderByDesc('id')->paginate(8)]);
+})->name('guests.projects.index');
+
+Route::get('projects/{project}', function (Project $project) {
+    return view('guests.projects.show', compact('project'));
+})->name('guests.projects.show');
 
 Route::middleware(['auth', 'verified'])
 ->name('admin.')
