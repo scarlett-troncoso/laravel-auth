eb00c48095562094ee6ebd934d64687b24605f96
scarlett-troncoso <scar.labussieri@gmail.com>
2024-05-29 18:01:15 +0200

    add relation one to many of type and Project,  HasMany and BelongsTo

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'cover_image', 'description', 'tools', 'project_url', 'source_code_url', 'slug'];
+    protected $fillable = ['title', 'cover_image', 'description', 'tools', 'project_url', 'source_code_url', 'slug', 'type_id'];
+
+    /**
+     * Get the type that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo /*Project apartiene(belongsTo) a un solo type*/
+    {
+        return $this->belongsTo(Type::class); /*restituisce un'istanza di questa relazione belongsTo*/
+    }
 }
