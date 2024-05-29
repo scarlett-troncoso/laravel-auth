00912025f774dd8797bebc5ad594a56ff5d5aee4
scarlett-troncoso <scar.labussieri@gmail.com>
2024-05-29 15:52:08 +0200

    create partial file for card of porjects

---
+++ resources/views/partials/card-projects.blade.php
@@ -0,0 +1,13 @@
+<div class="card h-100">
+    <a href="{{ route('projects.show', $project) }}">
+        @if (Str::startsWith($project->cover_image, 'https://'))
+            <img loading="lazy" class="card-img-top" src="{{ $project->cover_image }}" alt="">
+        @else
+            <img loading="lazy" class="card-img-top" src="{{ assets('storage/' . $project->cover_image) }}" alt="">
+        @endif
+    </a>
+    <div class="card-body">
+        <h2>{{ $project->title }}</h2>
+        <a class="btn btn-primary" href="{{ route('projects.show', $project) }}">View</a>
+    </div>
+</div>
