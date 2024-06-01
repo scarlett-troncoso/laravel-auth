cdbce3eb2231f8eecd82eaf37efbac551d285f64
scarlett-troncoso <scar.labussieri@gmail.com>
2024-06-01 23:16:27 +0200

    add data in table technologies

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['php', 'sql', 'JS', 'VueJS', 'Laravel'];
+
+        foreach ($technologies as $one_tech) {
+            $technology = new Technology();
+            $technology->name = $one_tech;
+            $technology->slug = Str::slug($one_tech, '-'); 
+            $technology->save();
+        }
     }
 }
