1271acf1679e7c06db055f510666bf1f90f3e133
scarlett-troncoso <scar.labussieri@gmail.com>
2024-06-05 19:12:13 +0200

    create routes of API for all project and only one project

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Project;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::get('projects', function(){
+    return response()->json([
+        'success' => true, 
+        'results' => Project::with(['type', 'technologies'])->orderByDesc('id')->paginate() // model Project con i metodi type(one to many) e technologies(many to many), con cui ha una relazione 
+        ]);
+    } );
+
+Route::get('projects/{project}', function($id){
+   $project = Project::with(['type', 'technologies'])->where('id', $id)->first();
+
+   if ($project) { // se esiste quell project 
+    return response()->json([ 
+        'success' => true,
+        'results' => $project
+    ]);
+   } else {
+    return response()->json([
+        'success' => false,
+        'results' => 'Project non trovato' //altrimente se non esiste mostrare questo messagio
+    ]);
+   }
 });
